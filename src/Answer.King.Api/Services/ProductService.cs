5e1d42c1a890866985264c6899262a1787d8de72
Fox Maltas <Fox.Maltas@answerdigital.com>
2023-01-12 17:12:07 +0000

    Fixed missing category save call

--- src/Answer.King.Api/Services/ProductService.cs
+++ src/Answer.King.Api/Services/ProductService.cs
@@ -66,19 +66,19 @@ public class ProductService : IProductService
             return null;
         }
 
-        var category = await this.Categories.Get(updateProduct.Category.Id);
-
-        if (category == null)
-        {
-            throw new ProductServiceException("The provided category is not valid.");
-        }
-
         product.Name = updateProduct.Name;
         product.Description = updateProduct.Description;
         product.Price = updateProduct.Price;
 
         if (product.Category.Id != updateProduct.Category.Id)
         {
+            var category = await this.Categories.Get(updateProduct.Category.Id);
+
+            if (category == null)
+            {
+                throw new ProductServiceException("The provided category is not valid.");
+            }
+
             var currentCategory = await this.Categories.Get(product.Category.Id);
 
             if (currentCategory == null)
@@ -87,6 +87,9 @@ public class ProductService : IProductService
             }
 
             currentCategory.RemoveProduct(new ProductId(product.Id));
+
+            await this.Categories.Save(currentCategory);
+
             product.SetCategory(new Category(category.Id, category.Name, category.Description));
         }
 
